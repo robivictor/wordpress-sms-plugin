81021dbcc7610e948f187c92623d4491b7427f30
lalatlove <lalatlove@ymail.com>
2015-10-02 07:20:05 -0700

    Fix menu issues

--- includes/class-plugin-name-activator.php
+++ includes/class-plugin-name-activator.php
@@ -31,46 +31,7 @@ class Plugin_Name_Activator {
 	 */
 	public static function activate() {
 // Hook for adding admin menus
-		add_action('admin_menu', 'mt_add_pages');
 
-// action function for above hook
-		function mt_add_pages()
-		{
-
-			// Add a new top-level menu (ill-advised):
-			add_menu_page(__('SMS Settings', 'menu-test'), __('C4TK SMS Plugin', 'menu-test'), 'manage_options', 'sms-settings', 'sms_settings', menu - icon - generic);
-
-			//add_menu_page( 'custom menu title', 'C4TK SMS Plugin' , 'manage_options', 'my-facebook-tags/phonebook.php', '', menu-icon-generic );
-
-			// Add a submenu to the custom top-level menu:
-			add_submenu_page('sms-settings', __('Phone Book', 'menu-test'), __('Phone Book', 'menu-test'), 'manage_options', 'phonebook', 'mt_sublevel_page');
-
-			// Add a second submenu to the custom top-level menu:
-			add_submenu_page('sms-settings', __('Messages', 'menu-test'), __('Messages', 'menu-test'), 'manage_options', 'messages', 'mt_sublevel_page2');
-		}
-
-// mt_toplevel_page() displays the page content for the custom Test Toplevel menu
-		function sms_settings()
-		{
-			echo "<h2>" . __('Test Settings', 'menu-test') . "</h2>";
-			echo "<a class='btn btn-succes'>Hello</a>";
-		}
-
-// mt_sublevel_page() displays the page content for the first submenu
-// of the custom Test Toplevel menu
-		function mt_sublevel_page()
-		{
-			echo "<h2>" . __('Test Settings', 'menu-test') . "</h2>";
-			echo "<a class='btn btn-succes'>Hello</a>";
-		}
-
-// mt_sublevel_page2() displays the page content for the second submenu
-// of the custom Test Toplevel menu
-		function mt_sublevel_page2()
-		{
-			echo "<h2>" . __('Test Settings', 'menu-test') . "</h2>";
-			echo "<a class='btn btn-succes'>Hello</a>";
-		}
  	//function create_table()
 		//{
 		global $mpdb;
