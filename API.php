735afe5b62357abc838e30388ad96fbd2d5b2dc0
bengeos <george.beng@gmail.com>
2015-10-11 19:51:42 +0300

    Configuring API to Fit Wordpress

--- API.php
+++ API.php
@@ -1,10 +1,11 @@
 <?php
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/ArifSMS/wp-load.php');
 global $wpdb;
 
 function get_Message(){
+    $response = '';
     // get message from SMSsync then save it to database
-    require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
+    require_once( $_SERVER['DOCUMENT_ROOT'] . '/ArifSMS/wp-load.php');
     include_once 'MyDataBase.php';
     global $wpdb;
 
@@ -13,17 +14,19 @@ function get_Message(){
     $Secret_Code = $result['conf_value'];
 
     $from = $_POST['from'];
+    $message_to = $_POST['sent_to'];
     $message = $_POST['message'];
     $secret = $_POST['secret'];
     $message_id = $_POST['message_id'];
     $device_id = $_POST['device_id'];
 
     if($secret == $Secret_Code){
-        if ((strlen($from) > 0) AND (strlen($message) > 0)  AND (strlen($message_id) > 0) AND (strlen($device_id) > 0)){
-            $message_table = new MyDataBase('sms_messages');
-            $message_table->insert(array('message_id'=>$message_id,'device_id'=>$device_id,'message_from'=>$from,'message'=>$message,'message_type'=>0));
-        }
+        $message_table = new MyDataBase('sms_messages');
+        write_message_to_file(json_encode(array('message_id'=>$message_id,'device_id'=>$device_id,'message_from'=>$from,'message'=>$message,'message_type'=>0)));
+        $message_table->insert(array('message_id'=>$message_id,'device_id'=>$device_id,'message_from'=>$from,'message'=>$message,'message_to'=>$message_to,'message_sent'=>1));
+        $response = json_encode(["payload"=> ["success"=>false,"error" => false]]);
     }
+    send_response($response);
 }
 function send_instant_message($to,$message)
 {
@@ -53,15 +56,14 @@ function send_task(){
     if (isset($_GET['task']) AND $_GET['task'] == 'send')
     {
         $msgs = array();
-        $message_table = new MyDataBase('sms_pending_messages');  // connect to a table that contains messages to be sent
-        $Pending_Messages = $result = (array) $message_table->get_all();
+        $message_table = new MyDataBase('sms_messages');  // connect to a table that contains messages to be sent
+        $Pending_Messages = $result = (array) $message_table->get_by(array('message_sent'=>1));
         foreach($Pending_Messages as $msg){
             $the_msg = (array) $msg;
-            array_push($msgs,["to"=>$the_msg['msg_to'],"message"=>$the_msg['message'],"uuid" => "1ba368bd-c467-4374-bf268"]);
+            array_push($msgs,["to"=>$the_msg['message_to'],"message"=>$the_msg['message'],"uuid" => "1ba368bd-c467-4374-bf268"]);
+            $message_table->update(array('message_sent'=>0),array('message_id'=>$the_msg['message_id']));
         }
         //delete all pending messages after sending to SMSSync
-        $message_table->delete_all();
-
         // Send JSON response back to SMSsync
         $response = json_encode(["payload"=>["success"=>true,"task"=>"send","secret" => $Secret_Code,"messages"=>array_values($msgs)]]);
         send_response($response);
@@ -82,10 +84,9 @@ function write_message_to_file($message)
     @fclose($fh);
 }
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    write_message_to_file(json_encode($_POST));
+    //write_message_to_file(json_encode($_POST));
     get_Message();
 }else{
-    write_message_to_file(json_encode($_GET));
-    echo json_encode('dfsfdgdf');
+    //write_message_to_file(json_encode($_GET));
     send_task();
 }
