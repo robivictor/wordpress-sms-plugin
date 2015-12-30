43f3084ca8e2f891fa2cb8525e2afe975be18c5c
Nahom Fikre <Nahom Fikre>
2015-12-31 00:28:48 +0400

    updated the dashboard

--- admin/partials/dashboard.php
+++ admin/partials/dashboard.php
@@ -11,9 +11,6 @@
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top" role="navigation">
                 <!-- Sidebar toggle button-->
-                <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-                    <span class="sr-only">Toggle navigation</span>
-                </a>
                 <!-- Navbar Right Menu -->
             </nav>
         </header>
@@ -23,7 +20,7 @@
             <section class="sidebar">
                 <!-- Sidebar user panel -->
                 <ul class="sidebar-menu">
-                    <li class="header">MAIN NAVIGATION</li>
+                    <li class="header"> <h3>MAIN NAVIGATION</h3></li>
                     <li>
                         <a ng-click="dash()">
                             <i class="fa fa-files-o"></i>
@@ -121,9 +118,10 @@
                 <div class="row" >
                     <!-- Left col -->
                     <div class="col-md-8" >
-                        <div class="box box-info" >
+                        <div class="box box-warning" >
                             <div class="box-header with-border">
                                 <h3 class="box-title">Phone book</h3>
+
                                 <div class="box-tools pull-right">
                                     <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                                     </button>
@@ -140,6 +138,7 @@
                                             <th>Phone</th>
                                             <th>Subscription Date</th>
                                         </tr>
+
                                         </thead>
                                         <tbody>
                                         <tr ng-repeat="x in subscribers">
@@ -155,51 +154,58 @@
                             </div>
                             <!-- /.box-body -->
                             <div class="box-footer clearfix">
-                                <a href="javascript::;" class="btn btn-sm btn-info btn-flat pull-left">Place New
-                                    Order</a>
-                                <a href="javascript::;" class="btn btn-sm btn-default btn-flat pull-right">View All
-                                    Orders</a>
+
+                                <a href="javascript::;" class="btn btn-sm btn-info btn-flat pull-right">View All
+                                    Contacts</a>
                             </div>
                             <!-- /.box-footer -->
                         </div>
                         <!-- /.box -->
                     </div>
                     <!-- /.col -->
+
                     <div class="col-md-4">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Add Contact</h3>
-                        </div><!-- /.box-header -->
-                        <!-- form start -->
-                        <form class="form-horizontal">
-                            <div class="box-body">
-                                <div class="form-group">
-                                    <label for="inputEmail3" class="col-sm-2 control-label">Name</label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputEmail3" placeholder="Name">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label>phone:</label>
-                                    <div class="input-group">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-phone"></i>
+                        <div class="box box-success" >
+                            <div class="box-header with-border">
+                                <h3 class="box-title">Add Contact</h3>
+                            </div><!-- /.box-header -->
+                            <!-- form start -->
+                            <form class="form-horizontal">
+                                <div class="box-body">
+                                    <div class="form-group">
+
+
+                                        <div class="col-sm-12">
+                                            <input type="" class="form-control" id="idname" placeholder="Name">
                                         </div>
-                                        <input type="text" class="form-control" data-inputmask="'mask': ['999-999-9999 [x99999]', '+099 99 99 9999[9]-9999']" data-mask>
-                                    </div><!-- /.input group -->
-                                </div><!-- /.form group -->
-                                <div >
-                                    <button type="submit" class="btn btn-info pull-right">Add</button>
-                                </div><!-- /.box-footer -->
-                                <!--/.box -->
-                            </div>
+                                    </div>
+                                    <div class="form-group">
+
+
+                                        <div class="col-sm-12">
+                                            <input type="" class="form-control" id="idphone" placeholder="Phone">
+                                        </div>
+                                    </div>
+
+
+                                    <div >
+                                        <button id="addbutton" type="submit" class="btn btn-info pull-right">Add</button>
+                                    </div><!-- /.box-footer -->
+                                </div><!--/.box -->
+
+                            </form>
                             <!-- /.col -->
+                        </div>
                     </div>
                     <!-- /.row -->
+                </div>
+                <!-- /.row -->
                     <div class="row" >
                         <div class="col-md-8">
                             <div class="box box-info">
                                 <div class="box-header with-border">
                                     <h3 class="box-title">Latest Messages</h3>
+
                                     <div class="box-tools pull-right">
                                         <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                                         </button>
@@ -221,11 +227,13 @@
                                             </thead>
                                             <tbody>
                                             <tr ng-repeat="x in message">
+
                                                 <td>{{ x.sender }}</td>
                                                 <td>{{ x.phone }}</td>
                                                 <td>{{ x.message}}</td>
                                                 <td class="label label-success">{{ x.status}}</td>
                                             </tr>
+
                                             </tbody>
                                         </table>
                                     </div>
@@ -233,6 +241,7 @@
                                 </div>
                                 <!-- /.box-body -->
                                 <div class="box-footer clearfix">
+
                                     <a href="javascript::;" class="btn btn-sm btn-default btn-flat pull-right">View All
                                         Messages</a>
                                 </div>
@@ -241,136 +250,34 @@
                             <!-- /.box -->
                         </div>
                         <div class="col-md-4">
-                            <div class="box box-warning direct-chat direct-chat-warning">
+                            <div class="box box-success">
                                 <div class="box-header with-border">
-                                    <h3 class="box-title">Send Message</h3>
+                                    <h3 class="box-title">Send Messages</h3>
+
                                     <div class="box-tools pull-right">
-                                        <span data-toggle="tooltip" title="3 New Messages" class="badge bg-yellow">3</span>
                                         <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                                         </button>
-                                        <button class="btn btn-box-tool" data-toggle="tooltip" title="Contacts"
-                                                data-widget="chat-pane-toggle"><i class="fa fa-comments"></i></button>
                                         <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
                                         </button>
                                     </div>
                                 </div>
-                                <!-- /.box-header -->
-                                <div class="box-body">
-                                    <!-- Conversations are loaded here -->
-                                    <div class="direct-chat-messages">
-                                    </div>
-                                    <!--/.direct-chat-messages-->
-                                    <!-- Contacts are loaded here -->
-                                    <div class="direct-chat-contacts">
-                                        <ul class="contacts-list">
-                                            <li>
-                                                <a href="#">
-                                                    <img class="contacts-list-img"
-                                                         src="/smsplugin/wp-content/plugins/ArifSms/admin/dist/img/user1-128x128.jpg">
-                                                    <div class="contacts-list-info">
-<span class="contacts-list-name">
-Count Dracula
-<small class="contacts-list-date pull-right">2/28/2015</small>
-</span>
-                                                        <span class="contacts-list-msg">How have you been? I was...</span>
-                                                    </div>
-                                                    <!-- /.contacts-list-info -->
-                                                </a>
-                                            </li>
-                                            <!-- End Contact Item -->
-                                            <li>
-                                                <a href="#">
-                                                    <img class="contacts-list-img"
-                                                         src="/smsplugin/wp-content/plugins/ArifSms/admin/dist/img/user7-128x128.jpg">
-                                                    <div class="contacts-list-info">
-<span class="contacts-list-name">
-Sarah Doe
-<small class="contacts-list-date pull-right">2/23/2015</small>
-</span>
-                                                        <span class="contacts-list-msg">I will be waiting for...</span>
-                                                    </div>
-                                                    <!-- /.contacts-list-info -->
-                                                </a>
-                                            </li>
-                                            <!-- End Contact Item -->
-                                            <li>
-                                                <a href="#">
-                                                    <img class="contacts-list-img"
-                                                         src="/smsplugin/wp-content/plugins/ArifSms/admin/dist/img/user3-128x128.jpg">
-                                                    <div class="contacts-list-info">
-<span class="contacts-list-name">
-Nadia Jolie
-<small class="contacts-list-date pull-right">2/20/2015</small>
-</span>
-                                                        <span class="contacts-list-msg">I'll call you back at...</span>
-                                                    </div>
-                                                    <!-- /.contacts-list-info -->
-                                                </a>
-                                            </li>
-                                            <!-- End Contact Item -->
-                                            <li>
-                                                <a href="#">
-                                                    <img class="contacts-list-img"
-                                                         src="/smsplugin/wp-content/plugins/ArifSms/admin/dist/img/user5-128x128.jpg">
-                                                    <div class="contacts-list-info">
-<span class="contacts-list-name">
-Nora S. Vans
-<small class="contacts-list-date pull-right">2/10/2015</small>
-</span>
-                                                        <span class="contacts-list-msg">Where is your new...</span>
-                                                    </div>
-                                                    <!-- /.contacts-list-info -->
-                                                </a>
-                                            </li>
-                                            <!-- End Contact Item -->
-                                            <li>
-                                                <a href="#">
-                                                    <img class="contacts-list-img"
-                                                         src="/smsplugin/wp-content/plugins/ArifSms/admin/dist/img/user6-128x128.jpg">
-                                                    <div class="contacts-list-info">
-<span class="contacts-list-name">
-John K.
-<small class="contacts-list-date pull-right">1/27/2015</small>
-</span>
-                                                        <span class="contacts-list-msg">Can I take a look at...</span>
-                                                    </div>
-                                                    <!-- /.contacts-list-info -->
-                                                </a>
-                                            </li>
-                                            <!-- End Contact Item -->
-                                            <li>
-                                                <a href="#">
-                                                    <img class="contacts-list-img"
-                                                         src="/smsplugin/wp-content/plugins/ArifSms/admin/dist/img/user8-128x128.jpg">
-                                                    <div class="contacts-list-info">
-<span class="contacts-list-name">
-Kenneth M.
-<small class="contacts-list-date pull-right">1/4/2015</small>
-</span>
-                                                        <span class="contacts-list-msg">Never mind I found...</span>
-                                                    </div>
-                                                    <!-- /.contacts-list-info -->
-                                                </a>
-                                            </li>
-                                            <!-- End Contact Item -->
-                                        </ul>
-                                        <!-- /.contatcts-list -->
-                                    </div>
-                                    <!-- /.direct-chat-pane -->
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Phone">
                                 </div>
-                                <!-- /.box-body -->
+
+                                <div class="form-group">
+                                <textarea id="compose-textarea" class="form-control" style="height: 100px">
+
+                                </textarea>
+                                </div>
+
                                 <div class="box-footer">
-                                    <form action="#" method="post">
-                                        <div class="input-group">
-                                            <input type="text" name="message" placeholder="Type Message ..."
-                                                   class="form-control">
-<span class="input-group-btn">
-<button type="button" class="btn btn-warning btn-flat">Send</button>
-</span>
-                                        </div>
-                                    </form>
-                                </div>
-                                <!-- /.box-footer-->
+                                    <div class="pull-right">
+                                        <button class="btn btn-default"><i class="fa fa-pencil"></i> Draft</button>
+                                        <button type="submit" class="btn btn-primary"><i class="fa fa-envelope-o"></i> Send</button>
+                                    </div>
+                                    <button class="btn btn-default"><i class="fa fa-times"></i> Discard</button>
+                                </div><!-- /.box-footer -->
                             </div>
                             <!--/.direct-chat -->
                         </div>
